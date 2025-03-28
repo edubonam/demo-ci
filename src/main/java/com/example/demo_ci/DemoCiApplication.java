9c002cf1c25439099d4fb62122abc0d44ab093b9
Eduardo Bonam Rissardi <eduardo_bonam@hotmail.com>
2025-03-28 15:37:38 -0300

    commit inicial

---
+++ src/main/java/com/example/demo_ci/DemoCiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_ci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCiApplication.class, args);
+	}
+
+}
