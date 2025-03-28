6166dcf5ee7fbb5cf6f6a68a98eb47ca7459bf37
Eduardo Bonam Rissardi <eduardo_bonam@hotmail.com>
2025-03-28 15:40:58 -0300

    adicao controller

--- src/main/java/com/example/demo_ci/DemoCiApplication.java
+++ src/main/java/com/example/demo_ci/DemoCiApplication.java
@@ -2,6 +2,8 @@ package com.example.demo_ci;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoCiApplication {
@@ -10,4 +12,14 @@ public class DemoCiApplication {
 		SpringApplication.run(DemoCiApplication.class, args);
 	}
 
+}
+
+
+@RestController
+class HelloController {
+
+	@GetMapping("/")
+	public String hello() {
+		return "Alegria, CI!";
+	}
 }
